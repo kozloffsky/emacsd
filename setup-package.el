601673b36c4be518023b39cec2c503387bf9c2cf
mkozlovsky <kozloffsky@gmail.com>
2013-11-26 14:27:03 +0200

    added package manager support and utils

---
+++ setup-package.el
@@ -0,0 +1,29 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packager") t)
+
+(package-initialize)
+
+(unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
+  (package-refresh-contents))
+
+
+(defun packages-install (packages)
+  (mapcar (lambda (package)
+	   (when (not (package-installed-p package))
+	     (package-install package)))
+	  packages)
+  (delete-other-windows))
+
+;; installation of packages
+
+(defun require-package (package &optional min-version no-refresh)
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contants) no-refresh)
+	(package-install package)
+      (progn
+	(package-refresh-contents)
+	(require-package package min-version t)))))
+
+(provide 'setup-package)
