97f13d8f3b90f432037e0ad0cb1e0a393a2cdea5
mkozlovsky <kozloffsky@gmail.com>
2013-11-26 12:17:49 +0200

    load path config

--- init.el
+++ init.el
@@ -10,6 +10,18 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
+;; No splash screen
+(setq inhibit-startup-message t)
+
+;Set path for dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path site-lisp-dir)
+
+
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
@@ -17,4 +29,6 @@
 ;; Key Mappings
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+
 
