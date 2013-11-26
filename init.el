78e2d61aa0f65d335beda9dc5bb652704fd2d921
mkozlovsky <kozloffsky@gmail.com>
2013-11-26 12:07:31 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,15 @@
+;;; IDO MODE
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+
+;; Visual enhance
+(menu-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
